43d338b19ef6dd54d6e63d9daa8805274ab37d71
Florian <herzog-florian@gmx.de>
2021-01-01 08:21:09 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 0)
+})
+radio.setGroup(1)
+let Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+Pixel_Array.showColor(neopixel.colors(NeoPixelColors.Green))
+basic.forever(function () {
+	
+})
