3d0d72779de1e3afdeaca45dc16243551156faf4
Florian <herzog-florian@gmx.de>
2021-01-01 08:30:16 +0100

    turn when received "1" on radio group 1

--- main.ts
+++ main.ts
@@ -1,17 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
-	
-})
-input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P1, 180)
-    pins.servoWritePin(AnalogPin.P2, 180)
+    if (receivedNumber == 1) {
+        pins.servoWritePin(AnalogPin.P1, 180)
+        pins.servoWritePin(AnalogPin.P2, 180)
+        basic.pause(1000)
+        pins.servoWritePin(AnalogPin.P1, 90)
+        pins.servoWritePin(AnalogPin.P2, 90)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
-})
-input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P1, 0)
-    pins.servoWritePin(AnalogPin.P2, 0)
 })
 radio.setGroup(1)
 let Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
